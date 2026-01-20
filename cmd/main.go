bbdd00d1b05e8a1bcf2d2bf14b30f13583500905
d.kurbangaliev <d.kurbangaliev@darrail.com>
2026-01-20 23:43:30 +0500

    изменены настройки. добавлены метрики.

--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,14 @@ import (
 	"net/http"
 
 	"github.com/gorilla/mux"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var httpCounter = prometheus.NewCounterVec(prometheus.CounterOpts{
+	Name: "http_request_total",
+	Help: "Total numbers of HTTP Requests"},
+	[]string{"path"},
 )
 
 func main() {
@@ -16,17 +24,23 @@ func main() {
 	staticPath := http.Dir("./web/assets")
 	fs := http.FileServer(staticPath)
 
+	prometheus.MustRegister(httpCounter)
+
 	r.HandleFunc("/", showIndexPage).Methods("GET")
 	r.HandleFunc("/services", showServicesPage).Methods("GET")
 	r.HandleFunc("/sustainableDevelopment", showSustainableDevelopment).Methods("GET")
 	r.HandleFunc("/news", showNews).Methods("GET")
 	r.HandleFunc("/contacts", showContacts).Methods("GET")
+
+	r.Handle("/metrics", promhttp.Handler())
+
 	r.PathPrefix(staticDir).Handler(http.StripPrefix(staticDir, fs)).Methods("GET")
 
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func showContacts(writer http.ResponseWriter, request *http.Request) {
+	httpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
 	tmpl, err := template.ParseFiles("web/templates/contacts.html")
 	if err != nil {
 		fmt.Printf("Error parsing contacts.html: %v \n", err)
@@ -40,6 +54,7 @@ func showContacts(writer http.ResponseWriter, request *http.Request) {
 }
 
 func showNews(writer http.ResponseWriter, request *http.Request) {
+	httpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
 	tmpl, err := template.ParseFiles("web/templates/news.html")
 	if err != nil {
 		fmt.Printf("Error parsing news.html: %v \n", err)
@@ -53,6 +68,7 @@ func showNews(writer http.ResponseWriter, request *http.Request) {
 }
 
 func showSustainableDevelopment(writer http.ResponseWriter, request *http.Request) {
+	httpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
 	tmpl, err := template.ParseFiles("web/templates/sustainableDevelopment.html")
 	if err != nil {
 		fmt.Printf("Error parsing sustainableDevelopment.html: %v \n", err)
@@ -66,6 +82,7 @@ func showSustainableDevelopment(writer http.ResponseWriter, request *http.Reques
 }
 
 func showServicesPage(writer http.ResponseWriter, request *http.Request) {
+	httpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
 	tmpl, err := template.ParseFiles("web/templates/services.html")
 	if err != nil {
 		fmt.Printf("Error parsing services.html: %v \n", err)
@@ -79,6 +96,7 @@ func showServicesPage(writer http.ResponseWriter, request *http.Request) {
 }
 
 func showIndexPage(writer http.ResponseWriter, r *http.Request) {
+	httpCounter.With(prometheus.Labels{"path": r.URL.Path}).Inc()
 	tmpl, err := template.ParseFiles("web/templates/index.html")
 	if err != nil {
 		fmt.Printf("Error parsing index.html: %v \n", err)
