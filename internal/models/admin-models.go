73707f6051d61e638b3dec72a2fcb9a574ab51b8
d.kurbangaliev <d.kurbangaliev@darrail.com>
2026-01-25 01:01:23 +0500

    remove unused items

--- internal/models/admin-models.go
+++ internal/models/admin-models.go
@@ -14,28 +14,3 @@ type ImageInfo struct {
 	Name string `json:"name"`
 	URL  string `json:"url"`
 }
-
-type NewsItem struct {
-	Id      uint   `json:"id"`
-	Title   string `json:"title"`
-	Content string `json:"content"`
-	Date    string `json:"date"`
-	Image   string `json:"image"`
-}
-
-type ManagerItem struct {
-	Id       uint   `json:"id"`
-	Name     string `json:"name"`
-	JobTitle string `json:"job_title"`
-	Address  string `json:"address"`
-	Phone    string `json:"phone"`
-	Email    string `json:"email"`
-	Schedule string `json:"schedule"`
-	Image    string `json:"image"`
-}
-
-type PartnerItem struct {
-	Id    uint   `json:"id"`
-	Name  string `json:"name"`
-	Image string `json:"image"`
-}
