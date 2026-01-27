4be96b75d21c69fa25eb7eef0eb5f20467d7a136
d.kurbangaliev <d.kurbangaliev@darrail.com>
2026-01-28 00:12:15 +0500

    добавлено создание базы данных и восстановление тестовых данных для демонстрации приложения

--- scripts/initdb.sh
+++ scripts/initdb.sh
@@ -1,33 +1,4 @@
 #!/bin/bash
 set -e
 
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-	CREATE USER finalwork;
-	CREATE DATABASE finalwork;
-	GRANT ALL PRIVILEGES ON DATABASE finalwork TO finalwork;
-	\c finalwork;
-  CREATE TABLE IF NOT EXISTS public.users
-  (
-      id bigint NOT NULL DEFAULT nextval('users_id_seq'::regclass),
-      created_at timestamp with time zone,
-      updated_at timestamp with time zone,
-      deleted_at timestamp with time zone,
-      login character varying(255) COLLATE pg_catalog."default",
-      password character varying(255) COLLATE pg_catalog."default",
-      CONSTRAINT users_pkey PRIMARY KEY (id)
-  )
-
-  TABLESPACE pg_default;
-
-  ALTER TABLE IF EXISTS public.users
-      OWNER to postgres;
-  -- Index: idx_users_deleted_at
-
-  -- DROP INDEX IF EXISTS public.idx_users_deleted_at;
-
-  CREATE INDEX IF NOT EXISTS idx_users_deleted_at
-      ON public.users USING btree
-      (deleted_at ASC NULLS LAST)
-      WITH (fillfactor=100, deduplicate_items=True)
-      TABLESPACE pg_default;
-EOSQL
+psql -v ON_ERROR_STOP=1 -U finalwork -d finalwork -f /docker-entrypoint-initdb.d/finalwork.sql;
