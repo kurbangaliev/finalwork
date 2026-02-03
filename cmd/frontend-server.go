13767553591f46b34f6d395300f55a793fc2a325
d.kurbangaliev <d.kurbangaliev@darrail.com>
2026-02-04 00:18:01 +0500

    use middleware functions for show pages

--- cmd/frontend-server.go
+++ cmd/frontend-server.go
@@ -3,6 +3,7 @@ package main
 import (
 	"finalwork/internal/db"
 	"finalwork/internal/handlers"
+	"finalwork/internal/models"
 	"fmt"
 	"log"
 	"net/http"
@@ -39,31 +40,53 @@ func main() {
 	prometheus.MustRegister(handlers.HttpCounter)
 
 	// Public Routes
-	router.HandleFunc("/", handlers.ShowIndexPage).Methods("GET")
-	router.HandleFunc("/services", handlers.ShowServicesPage).Methods("GET")
-	router.HandleFunc("/sustainableDevelopment", handlers.ShowSustainableDevelopment).Methods("GET")
+	//"web/templates/index.html"
+	//router.HandleFunc("/", handlers.ShowIndexPage).Methods("GET")
+	router.Handle("/", handlers.ShowTemplatePageGeneric[models.News]("web/templates/index.html", http.HandlerFunc(handlers.HandlerTemplate), db.SelectAll)).Methods("GET")
+	//"web/templates/services.html"
+	//router.HandleFunc("/services", handlers.ShowServicesPage).Methods("GET")
+	router.Handle("/services", handlers.ShowTemplatePage("web/templates/services.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
+	//"web/templates/sustainableDevelopment.html"
+	//router.HandleFunc("/sustainableDevelopment", handlers.ShowSustainableDevelopment).Methods("GET")
+	router.Handle("/sustainableDevelopment", handlers.ShowTemplatePage("web/templates/sustainableDevelopment.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
 	//	router.HandleFunc("/news", handlers.ShowNews).Methods("GET")
-	router.HandleFunc("/news", handlers.ShowNews).Methods("GET")
-	router.HandleFunc("/news/{id:[0-9]+}", handlers.ShowNews).Methods("GET")
-	router.HandleFunc("/contacts", handlers.ShowContacts).Methods("GET")
+	//router.HandleFunc("/news", handlers.ShowNews).Methods("GET")
+	//"web/templates/news.html"
+	router.Handle("/news", handlers.ShowTemplatePageParams("web/templates/news.html", http.HandlerFunc(handlers.HandlerTemplate), handlers.SelectDataNews)).Methods("GET")
+	//router.HandleFunc("/news/{id:[0-9]+}", handlers.ShowNews).Methods("GET")
+	router.Handle("/news/{id:[0-9]+}", handlers.ShowTemplatePageParams("web/templates/news.html", http.HandlerFunc(handlers.HandlerTemplate), handlers.SelectDataNews)).Methods("GET")
+	//"web/templates/contacts.html"
+	//router.HandleFunc("/contacts", handlers.ShowContacts).Methods("GET")
+	router.Handle("/contacts", handlers.ShowTemplatePageGeneric[models.Manager]("web/templates/contacts.html", http.HandlerFunc(handlers.HandlerTemplate), db.SelectAll)).Methods("GET")
 
 	//Prometheus handler
 	router.Handle("/metrics", promhttp.Handler())
 	router.PathPrefix(staticDir).Handler(http.StripPrefix(staticDir, fs)).Methods("GET")
 
 	// Authentification
-	router.HandleFunc("/login", handlers.ShowLoginPage).Methods("GET")
+	//	router.HandleFunc("/login", handlers.ShowLoginPage).Methods("GET")
+	router.Handle("/login", handlers.ShowTemplatePage("web/templates/admin/auth.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
 	router.HandleFunc("/logout", handlers.HandlerLogout).Methods("POST")
 
 	// Private Routes
 	protected := router.PathPrefix("/").Subrouter()
 	protected.Use(handlers.JWTAuth)
 	// Admin page handlers
-	protected.HandleFunc("/images", handlers.ShowImagesPage).Methods("GET")
-	protected.HandleFunc("/newsAdd", handlers.NewsAddPage).Methods("GET")
-	protected.HandleFunc("/newsBrowser", handlers.NewsBrowserPage).Methods("GET")
-	protected.HandleFunc("/managerAdd", handlers.ManagerAddPage).Methods("GET")
-	protected.HandleFunc("/managerBrowser", handlers.ManagerBrowserPage).Methods("GET")
+	//"web/templates/admin/images.html"
+	//protected.HandleFunc("/images", handlers.ShowImagesPage).Methods("GET")
+	protected.Handle("/images", handlers.ShowTemplatePage("web/templates/admin/images.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
+	//"web/templates/admin/newsAdd.html"
+	//protected.HandleFunc("/newsAdd", handlers.NewsAddPage).Methods("GET")
+	protected.Handle("/newsAdd", handlers.ShowTemplatePage("web/templates/admin/newsAdd.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
+	//"web/templates/admin/newsBrowser.html"
+	//protected.HandleFunc("/newsBrowser", handlers.NewsBrowserPage).Methods("GET")
+	protected.Handle("/newsBrowser", handlers.ShowTemplatePage("web/templates/admin/newsBrowser.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
+	//"web/templates/admin/managerAdd.html"
+	//protected.HandleFunc("/managerAdd", handlers.ManagerAddPage).Methods("GET")
+	protected.Handle("/managerAdd", handlers.ShowTemplatePage("web/templates/admin/managerAdd.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
+	//"web/templates/admin/managerBrowser.html"
+	//protected.HandleFunc("/managerBrowser", handlers.ManagerBrowserPage).Methods("GET")
+	protected.Handle("/managerBrowser", handlers.ShowTemplatePage("web/templates/admin/managerBrowser.html", http.HandlerFunc(handlers.HandlerTemplate))).Methods("GET")
 
 	log.Println("🚀 Frontend server started on http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", router))
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -28,7 +28,6 @@ func ShowContacts(writer http.ResponseWriter, request *http.Request) {
 		fmt.Printf("Error parsing contacts.html: %v \n", err)
 	}
 
-	//managers := db.SelectAllManagers()
 	managers, err := db.SelectAll[models.Manager]()
 	if err != nil {
 		http.Error(writer, "Error load managers", http.StatusInternalServerError)
@@ -111,6 +110,115 @@ func ShowSustainableDevelopment(writer http.ResponseWriter, request *http.Reques
 	}
 }
 
+func HandlerTemplate(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+}
+
+type GenericFunc[T comparable] func() ([]T, error)
+type DataFuncParams func(map[string]string) (any, error)
+
+func SelectDataNews(vars map[string]string) (any, error) {
+	strId := vars["id"]
+	var selectedId int
+
+	news, err := db.SelectAll[models.News]()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	selectedId, err = strconv.Atoi(strId)
+	if err != nil {
+		selectedId = 0
+	}
+	if selectedId > len(news) {
+		selectedId = 0
+	}
+
+	var selectedNews = models.News{
+		Model: gorm.Model{
+			ID: uint(selectedId),
+		},
+	}
+
+	if selectedNews, err = db.Select[models.News](selectedNews); err != nil {
+		log.Fatal(err)
+	}
+
+	var dataNews = DataNews{
+		AllNews:  news,
+		Selected: selectedNews,
+	}
+	return dataNews, nil
+}
+
+func ShowTemplatePage(templatePage string, handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("ShowTemplatePage [%s] \n]...", templatePage)
+		HttpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
+		tmpl, err := template.ParseFiles(templatePage)
+		if err != nil {
+			fmt.Printf("Error parsing services.html: %v \n", err)
+		}
+
+		err = tmpl.Execute(writer, nil)
+		if err != nil {
+			http.Error(writer, "Error rendering template page "+templatePage, http.StatusInternalServerError)
+			log.Printf("Error executing template: [%s] %v", templatePage, err)
+		}
+		handler.ServeHTTP(writer, request)
+	})
+}
+
+func ShowTemplatePageGeneric[T comparable](templatePage string, handler http.Handler, dataFunc GenericFunc[T]) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("ShowTemplatePageGeneric [%s]...", templatePage)
+		HttpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
+		tmpl, err := template.ParseFiles(templatePage)
+		if err != nil {
+			fmt.Printf("Error parsing services.html: %v \n", err)
+		}
+
+		data, err := dataFunc()
+		if err != nil {
+			http.Error(writer, "Error rendering template page "+templatePage, http.StatusInternalServerError)
+			log.Printf("Error executing template: [%s] %v", templatePage, err)
+		}
+
+		err = tmpl.Execute(writer, data)
+		if err != nil {
+			http.Error(writer, "Error rendering template page "+templatePage, http.StatusInternalServerError)
+			log.Printf("Error executing template: [%s] %v", templatePage, err)
+		}
+		handler.ServeHTTP(writer, request)
+	})
+}
+
+func ShowTemplatePageParams(templatePage string, handler http.Handler, dataFunc DataFuncParams) http.Handler {
+	return http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		log.Printf("ShowTemplatePageParams [%s]...", templatePage)
+		HttpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
+		vars := mux.Vars(request)
+		tmpl, err := template.ParseFiles(templatePage)
+		if err != nil {
+			fmt.Printf("Error parsing services.html: %v \n", err)
+		}
+
+		data, err := dataFunc(vars)
+
+		if err != nil {
+			http.Error(writer, "Error rendering template page "+templatePage, http.StatusInternalServerError)
+			log.Printf("Error executing template: [%s] %v", templatePage, err)
+		}
+
+		err = tmpl.Execute(writer, data)
+		if err != nil {
+			http.Error(writer, "Error rendering template page "+templatePage, http.StatusInternalServerError)
+			log.Printf("Error executing template: [%s] %v", templatePage, err)
+		}
+		handler.ServeHTTP(writer, request)
+	})
+}
+
 // ShowServicesPage Отображение страницы Услуги
 func ShowServicesPage(writer http.ResponseWriter, request *http.Request) {
 	HttpCounter.With(prometheus.Labels{"path": request.URL.Path}).Inc()
@@ -134,7 +242,6 @@ func ShowIndexPage(writer http.ResponseWriter, r *http.Request) {
 		fmt.Printf("Error parsing index.html: %v \n", err)
 	}
 
-	//	news := db.SelectAllNews()
 	news, _ := db.SelectAll[models.News]()
 
 	err = tmpl.Execute(writer, news)
